2345fe52d2d8d44d7f43e7e6ef5d319001a3cd85
Kevin Quintero <kevinstequintero@gmail.com>
2024-12-02 07:15:11 -0500

    a

--- demo_backend/src/main/java/com/example/demo/demo_backend/config/CorsConfig.java
+++ demo_backend/src/main/java/com/example/demo/demo_backend/config/CorsConfig.java
@@ -8,27 +8,16 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class CorsConfig {
-
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-
-        // Permite solicitudes de todos los orígenes (o puedes especificar un solo dominio si quieres restringir el acceso)
-        corsConfiguration.addAllowedOrigin("virutal-vault.netlify.app");  // Origen específico de Netlify
-
-        // Permite todos los encabezados
+        corsConfiguration.addAllowedOrigin("https://virtual-vault.netlify.app");
         corsConfiguration.addAllowedHeader("*");
-
-        // Permite todos los métodos
         corsConfiguration.addAllowedMethod("*");
-
-        // Permite credenciales (cookies o autenticación basada en sesiones)
         corsConfiguration.setAllowCredentials(true);
-
-        // Configuración para manejar preflight requests (OPTIONS)
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", corsConfiguration);
-
         return new CorsFilter(source);
     }
 }
+
